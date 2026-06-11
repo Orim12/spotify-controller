d9e57ad7cc85139eb4deb0399fa9b4d7c2e484fa
Orim12 <miro@mirovaassen.nl>
2026-06-11 15:07:28 +0200

    Update editUrl in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -118,7 +118,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Orim12/spotify-controller/tree/main/docs/',
+            'https://github.com/Orim12/spotify-controller/tree/main/',
         },
         blog: {
           showReadingTime: true,
